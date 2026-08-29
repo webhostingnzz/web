5428f7a5d6006e54fad1dce1ce7f0849cd806691
dilip1219 <dilip.udaya1219@gmail.com>
2026-08-30 02:00:48 +1200

    Add fast client-side navigation with hover prefetching, add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import GlobalScripts from './GlobalScripts';
 import PageTransition from './lib/PageTransition';
+import FastNav from './lib/FastNav';
 
 export const metadata = {
   metadataBase: new URL('https://webhosting.co.nz'),
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en-US">
       <body className="wp-singular page-template page-template-elementor_header_footer page wp-theme-hostiko theme-hostiko woocommerce-no-js elementor-default elementor-template-full-width elementor-kit-10 elementor-page">
         <GlobalScripts />
+        <FastNav />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
